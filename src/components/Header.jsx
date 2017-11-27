f694ac62b746c1eca99dc29ae1da0e47c6121c58
Dan Isacson <dan@themmobilelife.com>
2017-11-27 21:16:35 +0100

    new banner logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,56 +6,53 @@ import colors from '../utils/colors'
 
 const bannerStyle = {
   display: 'inline',
-  color: 'black'  
+  color: 'white',
+  borderStyle: 'solid',
+  borderWidth: '2px',
+  borderColor: 'white',
+  padding: '0 8px'
 }
 
 class Header extends Component {
   render() {
     return (
-      <div style={{backgroundColor: colors.dnaCyan}}>
-      <div style={{ margin: '0 auto', maxWidth: 750, padding: '1.25rem 1rem' }}>
-      <header>
-        <Link to='/' style={{
-          textShadow: 'none',
-          backgroundImage: 'none',
-          color: 'black'
-        }}>
-          <MediaQuery minWidth={440}>
-            {(matches) => {
-              if (matches) {
-                return <h3 style={bannerStyle}>Dan Isacson</h3>
-              } else {
-                return <h3 style={bannerStyle}>DI</h3>
-              }
-            }}
-          </MediaQuery>
-        </Link>
-        <MediaQuery minWidth={320}>
-          {(matches) => {
-            if (matches) {
-              return <BigList />
-            } else {
-              return <SmallList />
-            }
-          }}
-        </MediaQuery>
-      </header>
-      </div>
-      </div>
+      <div style={{ backgroundColor: colors.dnaCyan }}>
+        <div style={{ margin: '0 auto', maxWidth: 750, padding: '1.25rem 1rem' }}>
+          <header>
+            <Link to='/' style={{
+              backgroundImage: 'none',
+              color: 'white'
+            }}>
+              <h3 style={bannerStyle}>DI </h3>
+            </Link>
+            <MediaQuery minWidth={320}>
+              {(matches) => {
+                if (matches) {
+                  return <BigList />
+                } else {
+                  return <SmallList />
+                }
+              }}
+            </MediaQuery>
+          </header>
+        </div>
+      </div >
     )
   }
 }
 
 const SmallList = _ =>
-  <ul style={{   display: 'inline',  listStyle: 'none', float: 'right' }}>
+  <ul style={{ display: 'inline', listStyle: 'none', float: 'right' }}>
     <ListLink to='/portfolio/'>Ptfo</ListLink>
     <ListLink to='/blog/'>Blg</ListLink>
     <ListLink to='/about/'>Abt</ListLink>
   </ul>
 
 const BigList = _ =>
-  <ul style={{   display: 'inline',
-  listStyle: 'none', float: 'right' }}>
+  <ul style={{
+    display: 'inline',
+    listStyle: 'none', float: 'right'
+  }}>
     <ListLink to='/portfolio/'>Portfolio</ListLink>
     <ListLink to='/blog/'>Blog</ListLink>
     <ListLink to='/about/'>About</ListLink>
