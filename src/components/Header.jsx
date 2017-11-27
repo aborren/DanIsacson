b75f0ee667927d0d625f4300d4f8595abd092c59
Dan Isacson <dan@themmobilelife.com>
2017-11-27 21:47:52 +0100

    minor text fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ class Header extends Component {
               backgroundImage: 'none',
               color: 'white'
             }}>
-              <h3 style={bannerStyle}>DI </h3>
+              <h3 style={bannerStyle}>DI</h3>
             </Link>
             <MediaQuery minWidth={320}>
               {(matches) => {
